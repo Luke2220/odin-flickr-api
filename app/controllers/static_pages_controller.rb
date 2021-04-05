4cd03e994acc377e59fd4c2cc327c519d07a5303
Luke <kingluke2222@live.com>
2021-04-05 12:13:18 -0700

    project finished

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,7 +1,24 @@
 class StaticPagesController < ApplicationController
 
 def index
-    
+    if params[:flickr_user_id]
+        require 'flickr'
+        @flickr = Flickr.new ENV['flickr_api_key'],ENV['flickr_secret_key']
+        @list = @flickr.photos.search :user_id => params[:flickr_user_id]
+        @urls = Array.new
+        @list.each do |img|
+            @sizes = @flickr.photos.getSizes :photo_id => img.id
+            @urls << @sizes[6].source  
+        end
+        
+        p @flickr.photos.search :user_id => ENV['my_id']
+    end
+end
+
+private
+
+def static_page_params
+params.require(:static_pages).permit(:flickr_user_id)
 end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-resources :StaticPagesController
-root_to 'index#static_pages'
+resources :static_pages
+root to: 'static_pages#index'
 end
